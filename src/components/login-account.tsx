b7db6958cac2c98fe23518b88b853c2071747d20
lucaszebre <yakirisk@gmail.com>
2023-08-23 17:41:56 +0200

    kanban with a schadcn ui quality

---
+++ src/components/login-account.tsx
@@ -0,0 +1,109 @@
+import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button"
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from "@/components/ui/card"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import z from 'zod'
+import axios from "axios"
+import { useRouter } from "next/router"
+import { useState } from "react"
+import { FormDataRegister, SchemaLogin, schemaRegister } from "@/types"
+import supabase from "@/supabase"
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+export function LoginAccount() {
+    const Router = useRouter()
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const [success,setSuccess]=useState<boolean>(true)
+    const {register,handleSubmit,watch,formState: { errors },} = useForm({resolver: zodResolver(SchemaLogin),});
+    const watched = watch()
+    const onSubmit = async () => {
+        try {
+
+        const { data, error } = await supabase.auth.signInWithPassword({
+            email: email,
+            password: password,
+        })
+        
+        if(error){
+            console.error('Error login ',error.message)
+            setSuccess(false)
+        }else{
+            setSuccess(true)
+            Router.push('/')
+        }
+    
+       
+        } catch (error) {
+        console.error('Login error:', error);
+        // Handle registration error (display error message, etc.)
+        }
+    };
+
+        
+        
+        
+    return (
+        <form onSubmit={handleSubmit(()=>{
+            setEmail(watched.email);
+            setPassword(watched.password1);
+            onSubmit();
+        })}>
+        <Card className="border-none">
+        <CardHeader className="space-y-1">
+            <CardTitle className="text-2xl">Login an account</CardTitle>
+            <CardDescription>
+            Enter your email below to Log your account
+            </CardDescription>
+        </CardHeader>
+       
+        <CardContent className="grid gap-4">
+            <div className="grid grid-cols-2 gap-6 rounded">
+            <Button className="rounded " variant="outline">
+                <Icons.gitHub className="mr-2 h-4 w-4" />
+                Github
+            </Button>
+            <Button className="rounded" variant="outline">
+                <Icons.google className="mr-2 h-4 w-4" />
+                Google
+            </Button>
+            </div>
+            <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+                <span className="w-full border-t" />
+            </div>
+            <div className="relative flex justify-center text-xs uppercase">
+                <span className="bg-background px-2 text-muted-foreground">
+                Or continue with
+                </span>
+            </div>
+            </div>
+            
+            <div className="grid gap-2">
+            <Label htmlFor="email">Email</Label>
+            <Input {...register('email')} style={errors.email ? { border: '#EC5757 1px solid' } : {}} className="rounded" id="email" type="email" placeholder="m@example.com" />
+            {errors.email && <p className='text-red-500 font-thin'>{errors.email.message?.toString()}</p>}
+            </div>
+            <div className="grid gap-2">
+            <Label htmlFor="password">Password</Label>
+            <Input {...register('password')} className="rounded" style={errors.password ? { border: '#EC5757 1px solid' } : {}} id="password" type="password" />
+            {errors.password && <p className="text-red-500 font-thin">{errors.password?.message?.toString()}</p>}
+            </div>
+        {!success && <p className="text-red font-normal text-xs">invalid password </p>}
+        </CardContent>
+        <CardFooter>
+            <Button type="submit" className="w-full bg-black text-white hover:text-white hover:bg-gray-400 rounded">Login to the account</Button>
+        </CardFooter>
+        
+        </Card>
+       </form>)
+    }
---
+++ src/types/index.ts
@@ -0,0 +1,172 @@
+import z from "zod"
+export const schemaRegister = z.object({
+    email: z.string().email({ message: 'Invalid email format' }),
+    password1: z.string()
+        .min(8, { message: 'Password must be at least 8 characters long' })
+        .regex(/[A-Za-z]/, { message: 'Password must contain at least one letter' })
+        .regex(/[0-9]/, { message: 'Password must contain at least one digit' })
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: 'Password must contain at least one special character' }),
+    password2: z.string()
+        .min(8, { message: 'Password must be at least 8 characters long' })
+        .regex(/[A-Za-z]/, { message: 'Password must contain at least one letter' })
+        .regex(/[0-9]/, { message: 'Password must contain at least one digit' })
+        .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: 'Password must contain at least one special character' })
+        ,
+});
+
+
+export type BoardData = {
+    boards: Board[];
+}
+
+export type Data = {
+    boards: {
+        name: string;
+        id: string;
+        columns: {
+            name: string;
+            id: string;
+            tasks: {
+            id: string;
+            title: string;
+            description: string;
+            status: string;
+            subtasks: {
+                id: string;
+                title: string;
+                isCompleted: boolean;
+            }[];
+            }[];
+        }[];
+        }[];
+    };
+
+
+
+
+export type ColumnToRename = {
+    id: string;
+    name: string;
+};
+
+export type RequestBody = {
+    boardId: string;
+    boardName: string;
+    columnsToAdd: ColumnToRename[];
+    columnsToRename: ColumnToRename[];
+    columnsToRemove: string[];
+};
+
+
+
+export type ResponseError = {
+    message: string;
+};
+
+
+export type ColumnData = {
+    _id: string;
+    name: string;
+};
+
+export type TaskData = {
+    id: string;
+    title: string;
+    description: string;
+    status: string;
+    columnId: string;
+    subtasks: Array<{
+        title: string;
+        isCompleted: boolean;
+    }>;
+};
+
+
+
+
+export type Boards = {
+    id: string;
+    name: string;
+};
+
+export type AddBoardType = {
+    BoardName: string;
+};
+
+
+
+
+export interface Board {
+    _id: string;
+    name: string;
+    userId: string;
+    columns: Column[];
+    }
+    export interface ColumnAdd {
+        _id: string;
+        name: string;
+        boardId: string;
+        tasks: Task[];
+        add:boolean // Make sure it's 'tasks' instead of 'task'
+    }
+
+    export interface Column {
+        _id: string;
+        name: string;
+        boardId: string;
+        tasks: Task[];
+        add?:boolean // Make sure it's 'tasks' instead of 'task'
+    }
+    export interface Task {
+        _id: string;
+        title: string;
+        description: string;
+        columnId: string;
+        subtasks : Subtask[]
+    }
+    
+    export interface Subtask {
+        _id: string;
+        title: string;
+        isCompleted: boolean;
+        taskId: string;
+    }
+
+    export interface Subtasked{
+        title:string;
+        isCompleted:boolean
+    }
+
+
+
+  export   interface ListTaskProps {
+        tasks: Task[];
+        title: string;
+        NbList: number;
+        columnId:string
+        data: any;
+        columnIndex:number
+        }
+
+
+        export const SchemaLogin = z.object({
+            email: z.string().min(1,{ message: 'need a email' }),
+            password: z.string().min(8, { message: 'at least 8 characters long' })
+            .regex(/[A-Za-z]/, { message: ' must contain at least one letter' })
+            .regex(/[0-9]/, { message: ' must contain at least one digit' })
+            .regex(/[!@#$%^&*(),.?":{}|<>]/, { message: '  at least one special character' }),
+          });
+          
+          export interface FormDataRegister {
+            email: string;
+            password1: string;
+            password2: string;
+          }
+          
+          
+          
+          export const schemaProfile = z.object({
+            firstname: z.string().min(1, { message: 'cant be empty' }),
+            lastname: z.string().min(1, { message: 'cant be empty' }),
+            email: z.string().email({ message: 'Invalid email format' }),
+            });
